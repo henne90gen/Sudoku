9bd91c6d388b126d3c29d73ccd6aeb0d5c7f7d94
henne90gen <henne90gen@gmail.com>
2016-10-18 23:13:08 +0200

    Adding swt grid

--- src/sudoku_new/visualizer/SWTVisualizer.java
+++ src/sudoku_new/visualizer/SWTVisualizer.java
@@ -1,8 +1,12 @@
 package sudoku_new.visualizer;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import sudoku_new.Sudoku;
 
 /**
@@ -10,9 +14,11 @@ import sudoku_new.Sudoku;
  */
 public class SWTVisualizer extends Visualizer {
 
+    private static final int SHELL_OPTIONS = SWT.SHELL_TRIM;
+
     private Shell shell;
 
-    private static final int SHELL_OPTIONS = SWT.SHELL_TRIM;
+    private Text[] grid;
 
     public SWTVisualizer(Sudoku sudoku) {
         super(sudoku);
@@ -22,8 +28,11 @@ public class SWTVisualizer extends Visualizer {
         new Thread(() -> {
             Display display = new Display();
             shell = new Shell(display, SHELL_OPTIONS);
-            shell.open();
+            shell.setLayout(new FormLayout());
 
+            addComponents();
+
+            shell.open();
             while (!shell.isDisposed()) {
                 if (!display.readAndDispatch()) {
                     display.sleep();
@@ -31,6 +40,30 @@ public class SWTVisualizer extends Visualizer {
             }
             display.dispose();
         }).start();
+    }
+
+    private void addComponents() {
+        grid = new Text[81];
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                Text tmp = new Text(shell, SWT.BORDER);
+                FormData tmpData = new FormData();
+                if (row == 0) {
+                    tmpData.top = new FormAttachment();
+                } else {
+                    tmpData.top = new FormAttachment(grid[(row - 1) * 9 + col]);
+                }
+                if (col == 0) {
+                    tmpData.left = new FormAttachment();
+                } else {
+                    tmpData.left = new FormAttachment(grid[row * 9 + (col - 1)]);
+                }
+                tmpData.width = 25;
+                tmpData.height = 25;
+                tmp.setLayoutData(tmpData);
+                grid[row * 9 + col] = tmp;
+            }
+        }
     }
 
     @Override
